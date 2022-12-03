815c11feb22f6492332e1bbb75b2db271a60bd6c
osaizar <oisaizar@gmail.com>
2022-12-03 21:00:36 +0100

    typo

--- ssh-kms.py
+++ ssh-kms.py
@@ -4,8 +4,8 @@ from flask import Flask, request, jsonify, abort
 import re
 import json
 
-KEY_FILE = "ssh-keys.json"
-SCRIPT_FILE = "get-ssh-keys.py"
+KEY_FILE = "/config/ssh-keys.json"
+SCRIPT_FILE = "/app/get-ssh-keys.py"
 
 KEY_FORMAT = """
 [
@@ -44,7 +44,7 @@ def get_keys():
         except Exception as e:
             print(e)
             abort(400)
-            
+
         keys = filter_keys(data["user"], data["hostname"])
 
         return jsonify({"ssh-keys" : [k["ssh-key"] for k in keys]}), 200
